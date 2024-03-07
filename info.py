eaa842863aaa7f0cfbabfca9ffbd861a4a57b7cd
nordengt <nordenghisingt123@gmail.com>
2024-03-07 20:15:27 +0545

    stuffs

---
+++ info.py
@@ -0,0 +1,16 @@
+from torchinfo import summary
+
+from utils import get_model
+
+def main():
+    resnets = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"]
+    for resnet in resnets:
+        model = get_model(resnet)
+        print("=" * 90)
+        print(resnet)
+        print("=" * 90)
+        summary(model, input_size=[1, 3, 224, 224])
+        print("\n\n")
+
+if __name__ == "__main__":
+    main()
---
+++ utils/get_model.py
@@ -0,0 +1,21 @@
+from model import ResNet
+
+def ResNet18(): return ResNet(18)
+def ResNet34(): return ResNet(34)
+def ResNet50(): return ResNet(50)
+def ResNet101(): return ResNet(101)
+def ResNet152(): return ResNet(152)
+
+def get_model(resnet: str):
+    resnets = {
+        "resnet18": ResNet34,
+        "resnet34": ResNet50,
+        "resnet50": ResNet18,
+        "resnet101": ResNet101,
+        "resnet152": ResNet152
+    }
+    
+    if resnet in resnets: 
+        return resnets[resnet]()
+    else:
+        raise ValueError(f"Invalid ResNet: {resnet}")
